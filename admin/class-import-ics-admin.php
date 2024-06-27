93277efd7ac1b7921b5d0e5f5a297a6a8395a189
Alexander Bigga <alexander.bigga@verdigado.com>
2024-06-28 00:30:25 +0200

    PHPCS: fix coding standard

--- admin/class-import-ics-admin.php
+++ admin/class-import-ics-admin.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The admin-specific functionality of the plugin.
  *
@@ -10,7 +9,7 @@
  * @subpackage Import_Ics/admin
  */
 
- /**
+/**
  * The admin-specific functionality of the plugin.
  *
  * Defines the Import ICS, version, and two examples hooks for how to
@@ -40,23 +39,20 @@ class Import_Ics_Admin {
 	 */
 	private $version;
 
-	private $import_ics_events;
-
 	/**
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name      The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name      The name of this plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
-		add_action('admin_menu', array( $this, 'addPluginAdminMenu' ), 9);
-		add_action('admin_init', array( $this, 'registerAndBuildFields'));
-
+		add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ), 9 );
+		add_action( 'admin_init', array( $this, 'register_and_build_fields' ) );
 	}
 
 	/**
@@ -79,7 +75,6 @@ class Import_Ics_Admin {
 		 */
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/import-ics-admin.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -102,7 +97,6 @@ class Import_Ics_Admin {
 		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/import-ics-admin.js', array( 'jquery' ), $this->version, false );
-
 	}
 
 	/**
@@ -110,13 +104,10 @@ class Import_Ics_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function addPluginAdminMenu() {
+	public function add_plugin_admin_menu() {
 
-		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-		add_menu_page( $this->plugin_name, 'Import ICS', 'administrator', $this->plugin_name, array( $this, 'displayPluginAdminDashboard' ), 'dashicons-calendar', 26 );
-
-		//add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
-		add_submenu_page( $this->plugin_name, 'Import ICS Settings', 'Settings', 'administrator', $this->plugin_name.'-settings', array( $this, 'displayPluginAdminSettings' ));
+		add_menu_page( $this->plugin_name, 'Import ICS', 'edit_pages', $this->plugin_name, array( $this, 'display_plugin_admin_dashboard' ), 'dashicons-calendar', 26 );
+		add_submenu_page( $this->plugin_name, 'Import ICS Settings', 'Settings', 'edit_pages', $this->plugin_name . '-settings', array( $this, 'display_plugin_admin_settings' ) );
 	}
 
 	/**
@@ -124,76 +115,54 @@ class Import_Ics_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-	public function displayPluginAdminDashboard() {
+	public function display_plugin_admin_dashboard() {
 		require_once 'partials/' . $this->plugin_name . '-admin-display.php';
-  	}
+	}
 
 	/**
 	 * Include admin settings partial.
 	 *
 	 * @since    1.0.0
 	 */
-	public function displayPluginAdminSettings() {
-		// set this var to be used in the settings-display view
-		$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'general';
-		if(isset($_GET['error_message'])){
-			add_action('admin_notices', array($this,'importIcsSettingsMessages'));
-			do_action( 'admin_notices', $_GET['error_message'] );
-		}
-		require_once 'partials/'.$this->plugin_name.'-admin-settings-display.php';
+	public function display_plugin_admin_settings() {
+		require_once 'partials/' . $this->plugin_name . '-admin-settings-display.php';
 	}
 
-	public function importIcsSettingsMessages($error_message){
-		switch ($error_message) {
-			case '1':
-				$message = __( 'There was an error adding this setting. Please try again.  If this persists, shoot us an email.', 'my-text-domain' );
-				$err_code = esc_attr( 'import_ics_example_setting' );
-				$setting_field = 'import_ics_example_setting';
-				break;
-		}
-		$type = 'error';
-		add_settings_error(
-			   $setting_field,
-			   $err_code,
-			   $message,
-			   $type
-		   );
-	}
-
-	public function registerAndBuildFields() {
+	/**
+	 * Register and add event settings
+	 */
+	public function register_and_build_fields(): void {
 		/**
 		 * First, we add_settings_section. This is necessary since all future settings must belong to one.
 		 * Second, add_settings_field
 		 * Third, register_setting
 		 */
 		add_settings_section(
-			// ID used to identify this section and with which to register options
+			// ID used to identify this section and with which to register options.
 			'import_ics_general_section',
-			// Title to be displayed on the administration page
-			esc_html__('ICS calendar configuration', 'import-ics'),
-			// Callback used to render the description of the section
-			array($this, 'import_ics_display_general_account'),
-			// Page on which to add this section of options
+			// Title to be displayed on the administration page.
+			esc_html__( 'ICS calendar configuration', 'import-ics' ),
+			// Callback used to render the description of the section.
+			array( $this, 'import_ics_display_general_account' ),
+			// Page on which to add this section of options.
 			'import_ics_general_settings'
 		);
 
-		// import_ics_setting_1_url
 		add_settings_field(
 			'import_ics_setting_1_url',
-			esc_html__('URL to the calendar ics file', 'import-ics'),
-			array($this, 'import_ics_render_settings_field'),
+			esc_html__( 'URL to the calendar ics file', 'import-ics' ),
+			array( $this, 'import_ics_render_settings_field' ),
 			'import_ics_general_settings',
 			'import_ics_general_section',
-			array (
-				'type' => 'input',
-				'subtype' => 'text',
-				'id' => 'import_ics_setting_1_url',
-				'name' => 'import_ics_setting_1_url',
+			array(
+				'type'             => 'input',
+				'subtype'          => 'text',
+				'id'               => 'import_ics_setting_1_url',
+				'name'             => 'import_ics_setting_1_url',
 				'get_options_list' => '',
-				'value_type'=>'normal',
-				'wp_data' => 'option',
-				'required' => true,
-				'size' => 120
+				'wp_data'          => 'option',
+				'required'         => true,
+				'size'             => 120,
 			)
 		);
 
@@ -202,34 +171,32 @@ class Import_Ics_Admin {
 			'import_ics_setting_1_url'
 		);
 
-		// import_ics_setting_1_interval
 		$select_options = array(
-			'3600' => esc_html__('1 Hour', 'import-ics'),
-			'7200' => esc_html__('2 Hours', 'import-ics'),
-			'43200' => esc_html__('12 Hours', 'import-ics'),
-			'86400' => esc_html__('24 Hours', 'import-ics'),
+			'3600'  => esc_html__( '1 Hour', 'import-ics' ),
+			'7200'  => esc_html__( '2 Hours', 'import-ics' ),
+			'43200' => esc_html__( '12 Hours', 'import-ics' ),
+			'86400' => esc_html__( '24 Hours', 'import-ics' ),
 		);
 
-		if (defined('WP_DEBUG') && WP_DEBUG) {
-			$select_options['2'] = esc_html__('Immediately (Development)', 'import-ics');
+		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			$select_options['2'] = esc_html__( 'Immediately (Development)', 'import-ics' );
 		}
 
 		add_settings_field(
 			'import_ics_setting_1_interval',
-			esc_html__('Renew import interval', 'import-ics'),
-			array($this, 'import_ics_render_settings_field'),
+			esc_html__( 'Renew import interval', 'import-ics' ),
+			array( $this, 'import_ics_render_settings_field' ),
 			'import_ics_general_settings',
 			'import_ics_general_section',
-			array (
-				'type' => 'select',
-				'subtype' => 'text',
-				'id' => 'import_ics_setting_1_interval',
-				'name' => 'import_ics_setting_1_interval',
+			array(
+				'type'           => 'select',
+				'subtype'        => 'text',
+				'id'             => 'import_ics_setting_1_interval',
+				'name'           => 'import_ics_setting_1_interval',
 				'select_options' => $select_options,
-				'default' => '43200',
-				'value_type'=>'normal',
-				'wp_data' => 'option',
-				'required' => true,
+				'default'        => '43200',
+				'wp_data'        => 'option',
+				'required'       => true,
 			)
 		);
 
@@ -238,24 +205,22 @@ class Import_Ics_Admin {
 			'import_ics_setting_1_interval'
 		);
 
-		// import_ics_setting_interval_before
 		add_settings_field(
 			'import_ics_setting_interval_before',
-			esc_html__('Time interval: days before today', 'import-ics'),
-			array($this, 'import_ics_render_settings_field'),
+			esc_html__( 'Time interval: days before today', 'import-ics' ),
+			array( $this, 'import_ics_render_settings_field' ),
 			'import_ics_general_settings',
 			'import_ics_general_section',
-			array (
-				'type' => 'input',
-				'subtype' => 'text',
-				'id' => 'import_ics_setting_interval_before',
-				'name' => 'import_ics_setting_interval_before',
-				'default' => '15',
+			array(
+				'type'             => 'input',
+				'subtype'          => 'text',
+				'id'               => 'import_ics_setting_interval_before',
+				'name'             => 'import_ics_setting_interval_before',
+				'default'          => '15',
 				'get_options_list' => '',
-				'value_type'=>'normal',
-				'wp_data' => 'option',
-				'required' => true,
-				'size' => 5
+				'wp_data'          => 'option',
+				'required'         => true,
+				'size'             => 5,
 			)
 		);
 
@@ -264,24 +229,22 @@ class Import_Ics_Admin {
 			'import_ics_setting_interval_before'
 		);
 
-		// import_ics_setting_interval_after
 		add_settings_field(
 			'import_ics_setting_interval_after',
-			esc_html__('Time interval: days after today', 'import-ics'),
-			array($this, 'import_ics_render_settings_field'),
+			esc_html__( 'Time interval: days after today', 'import-ics' ),
+			array( $this, 'import_ics_render_settings_field' ),
 			'import_ics_general_settings',
 			'import_ics_general_section',
-			array (
-				'type' => 'input',
-				'subtype' => 'text',
-				'id' => 'import_ics_setting_interval_after',
-				'name' => 'import_ics_setting_interval_after',
-				'default' => '366',
+			array(
+				'type'             => 'input',
+				'subtype'          => 'text',
+				'id'               => 'import_ics_setting_interval_after',
+				'name'             => 'import_ics_setting_interval_after',
+				'default'          => '366',
 				'get_options_list' => '',
-				'value_type'=>'normal',
-				'wp_data' => 'option',
-				'required' => true,
-				'size' => 5
+				'wp_data'          => 'option',
+				'required'         => true,
+				'size'             => 5,
 			)
 		);
 
@@ -289,7 +252,6 @@ class Import_Ics_Admin {
 			'import_ics_general_settings',
 			'import_ics_setting_interval_after'
 		);
-
 	}
 
 
@@ -299,74 +261,66 @@ class Import_Ics_Admin {
 	 * @since    1.0.0
 	 */
 	public function import_ics_display_general_account() {
-		echo '<p>' . esc_html__('Enter the ICS download links and configure the renewal interval.', 'import-ics') . '</p>';
+		echo '<p>' . esc_html__( 'Enter the ICS download links and configure the renewal interval.', 'import-ics' ) . '</p>';
 	}
 
-	public function import_ics_render_settings_field($args) {
-		/* EXAMPLE INPUT
-							'type'      => 'input',
-							'subtype'   => '',
-							'id'    => $this->plugin_name.'_example_setting',
-							'name'      => $this->plugin_name.'_example_setting',
-							'required' => 'required="required"',
-							'get_option_list' => "",
-								'value_type' = serialized OR normal,
-		'wp_data'=>(option or post_meta),
-		'post_id' =>
-		*/
-		if($args['wp_data'] == 'option'){
-			$wp_data_value = get_option($args['name']);
-		} elseif($args['wp_data'] == 'post_meta'){
-			$wp_data_value = get_post_meta($args['post_id'], $args['name'], true );
+	/**
+	 * Render the given form field
+	 *
+	 * @param Array $args The field data array.
+	 */
+	public function import_ics_render_settings_field( $args ) {
+
+		if ( 'option' === $args['wp_data'] ) {
+			$wp_data_value = get_option( $args['name'] );
+		} elseif ( 'post_meta' === $args['wp_data'] ) {
+			$wp_data_value = get_post_meta( $args['post_id'], $args['name'], true );
 		}
 
-		switch ($args['type']) {
+		switch ( $args['type'] ) {
 
 			case 'input':
-					$value = ($args['value_type'] == 'serialized') ? serialize($wp_data_value) : $wp_data_value;
-					if (empty($value) && isset($args['default'])) {
-						$value = $args['default'];
-					}
-					if ($args['subtype'] != 'checkbox') {
-						$prependStart = (isset($args['prepend_value'])) ? '<div class="input-prepend"> <span class="add-on">'.$args['prepend_value'].'</span>' : '';
-						$prependEnd = (isset($args['prepend_value'])) ? '</div>' : '';
-						$step = (isset($args['step'])) ? 'step="'.$args['step'].'"' : '';
-						$min = (isset($args['min'])) ? 'min="'.$args['min'].'"' : '';
-						$max = (isset($args['max'])) ? 'max="'.$args['max'].'"' : '';
-						$required = (isset($args['required'])) ? 'required="required"' : '';
-						if (isset($args['disabled'])) {
-								// hide the actual input bc if it was just a disabled input the info saved in the database would be wrong - bc it would pass empty values and wipe the actual information
-								echo $prependStart.'<input type="'.$args['subtype'].'" id="'.$args['id'].'_disabled" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'_disabled" size="40" disabled value="' . esc_attr($value) . '" /><input type="hidden" id="'.$args['id'].'" '.$step.' '.$max.' '.$min.' name="'.$args['name'].'" size="40" value="' . esc_attr($value) . '" />'.$prependEnd;
-						} else {
-								echo $prependStart . '<input type="' . $args['subtype'] . '" id="' . $args['id'] . '" ' . $required . ' ' . $step . ' ' . $max . ' ' . $min . ' name="' . $args['name'] . '" size="' . ($args['size'] ?? '40') . '" value="' . esc_attr($value) . '" />' . $prependEnd;
-						}
-						/*<input required="required" '.$disabled.' type="number" step="any" id="'.$this->plugin_name.'_cost2" name="'.$this->plugin_name.'_cost2" value="' . esc_attr( $cost ) . '" size="25" /><input type="hidden" id="'.$this->plugin_name.'_cost" step="any" name="'.$this->plugin_name.'_cost" value="' . esc_attr( $cost ) . '" />*/
-					} else {
-							$checked = ($value) ? 'checked' : '';
-							echo '<input type="'.$args['subtype'].'" id="'.$args['id'].'" "' . $required . '" name="' . $args['name'] . '" size="' . ($args['size'] ?? '40') . '" value="1" '.$checked.' />';
-					}
-					break;
-			case 'select':
-				$value = ($args['value_type'] == 'serialized') ? serialize($wp_data_value) : $wp_data_value;
-				if (empty($value) && isset($args['default'])) {
+					$value = $wp_data_value;
+				if ( empty( $value ) && isset( $args['default'] ) ) {
 					$value = $args['default'];
 				}
-				echo '<select name="' . $args['name'] . '" id="' . $args['id'] . '">';
-				foreach ($args['select_options'] as $key => $option) {
-					if ($value == $key) {
+				if ( 'checkbox' !== $args['subtype'] ) {
+					$prepend_start = ( isset( $args['prepend_value'] ) ) ? '<div class="input-prepend"> <span class="add-on">' . esc_attr( $args['prepend_value'] ) . '</span>' : '';
+					$prepend_end   = ( isset( $args['prepend_value'] ) ) ? '</div>' : '';
+					$step          = ( isset( $args['step'] ) ) ? 'step="' . $args['step'] . '"' : '';
+					$min           = ( isset( $args['min'] ) ) ? 'min="' . $args['min'] . '"' : '';
+					$max           = ( isset( $args['max'] ) ) ? 'max="' . $args['max'] . '"' : '';
+					$required      = ( isset( $args['required'] ) ) ? 'required="required"' : '';
+					if ( isset( $args['disabled'] ) ) {
+							// hide the actual input bc if it was just a disabled input the info saved in the database would be wrong - bc it would pass empty values and wipe the actual information.
+							echo wp_kses_post( $prepend_start ) . '<input type="' . esc_attr( $args['subtype'] ) . '" id="' . esc_attr( $args['id'] ) . '_disabled" ' . esc_attr( $step ) . ' ' . esc_attr( $max ) . ' ' . esc_attr( $min ) . ' name="' . esc_attr( $args['name'] ) . '_disabled" size="40" disabled value="' . esc_attr( $value ) . '" /><input type="hidden" id="' . esc_attr( $args['id'] ) . '" ' . esc_attr( $step ) . ' ' . esc_attr( $max ) . ' ' . esc_attr( $min ) . ' name="' . esc_attr( $args['name'] ) . '" size="40" value="' . esc_attr( $value ) . '" />' . wp_kses_post( $prepend_end );
+					} else {
+							echo wp_kses_post( $prepend_start ) . '<input type="' . esc_attr( $args['subtype'] ) . '" id="' . esc_attr( $args['id'] ) . '" ' . esc_attr( $required ) . ' ' . esc_attr( $step ) . ' ' . esc_attr( $max ) . ' ' . esc_attr( $min ) . ' name="' . esc_attr( $args['name'] ) . '" size="' . esc_attr( $args['size'] ?? '40' ) . '" value="' . esc_attr( $value ) . '" />' . wp_kses_post( $prepend_end );
+					}
+				} else {
+						$checked = ( $value ) ? 'checked' : '';
+						echo '<input type="' . esc_attr( $args['subtype'] ) . '" id="' . esc_attr( $args['id'] ) . '" "' . esc_attr( $required ) . '" name="' . esc_attr( $args['name'] ) . '" size="' . esc_attr( $args['size'] ?? '40' ) . '" value="1" ' . esc_attr( $checked ) . ' />';
+				}
+				break;
+			case 'select':
+				$value = $wp_data_value;
+				if ( empty( $value ) && isset( $args['default'] ) ) {
+					$value = $args['default'];
+				}
+				echo '<select name="' . esc_attr( $args['name'] ) . '" id="' . esc_attr( $args['id'] ) . '">';
+				foreach ( $args['select_options'] as $key => $option ) {
+					if ( (int) $value === $key ) {
 						$selected = 'selected';
 					} else {
 						$selected = '';
 					}
-					echo '<option value="' . $key . '" ' . $selected . '>' . $option . '</option>';
+					echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_attr( $option ) . '</option>';
 				}
 				echo '</select>';
 				break;
 			default:
-					# code...
-					break;
+					// code...
+				break;
 		}
-
 	}
-
 }
